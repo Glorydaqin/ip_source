ad0518929f9c2a009acf24176c0cffa9bdfb27ad
qinwei <692860800@qq.com>
2021-03-09 16:33:55 +0800

    更新到laravel 7

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -7,14 +7,16 @@ use App\AdminUsers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Input;
 
 class LoginController extends Controller
 {
     //
     /*登录*/
-    public function login(){
-        if($input=Input::all()){
+    public function login(Request $request)
+    {
+        if ($request->method() == "POST") {
+            $input = $request->all();
+
             $rules = [
                 "user_name" => 'required',
                 "user_pass" => 'required',
@@ -24,16 +26,17 @@ class LoginController extends Controller
                 'user_pass.required' => '密码不能为空'
             ];
 
-            $validator = Validator::make(Input::all(), $rules, $messages);
-            if($validator->fails()) {
+            $validator = Validator::make($request->all(), $rules, $messages);
+            if ($validator->fails()) {
                 return back()->withErrors($validator);
             } else {
 
-                $adminUser=AdminUsers::where(['user_name'=>$input['user_name']])->first();
-                if(Crypt::decrypt($adminUser->user_pass)==$input['user_pass']){
-                    session(['user_name'=>$input['user_name']]);
+                $adminUser = AdminUsers::where(['user_name' => $input['user_name']])->first();
+
+                if ($adminUser && Crypt::decrypt($adminUser->user_pass) == $input['user_pass']) {
+                    session(['user_name' => $input['user_name']]);
                     return redirect('admin/index');
-                }else{
+                } else {
                     return redirect("admin/login/login");
                 }
 
@@ -43,14 +46,15 @@ class LoginController extends Controller
         return view("admin/login/login");
     }
 
-    public function logout(){
-        session(['user_name'=>null]);
+    public function logout()
+    {
+        session(['user_name' => null]);
         return redirect("admin/login");
     }
 
     public function resetPassword()
     {
-        AdminUsers::where("id",1)->update(['user_pass'=>Crypt::encrypt("admin")]);
+        AdminUsers::where("id", 1)->update(['user_pass' => Crypt::encrypt("admin")]);
         echo 1;
     }
 }
